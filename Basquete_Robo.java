bc992c6d1dd4e080a4234d314139b2021250311e
lucasgarrix <lucashacking123@hotmail.com>
2021-02-08 12:44:35 -0300

    basquete de robôs

---
+++ Basquete_Robo.java
@@ -0,0 +1,24 @@
+package estdados;
+public class Basquete_Robo {
+    Fig D;
+    public void Distancia(int valor){
+        D = new Fig();
+        D.valor = valor;
+        System.out.println(D.valor);
+        System.out.println("--Possíveis Pontos--");
+    }
+    public void PontLancamento(){
+        if(D.valor <= 800){
+            System.out.println("1");
+        }else if(D.valor > 800 && D.valor <= 1400){
+            System.out.println("2");
+        }else{
+            System.out.println("3");
+        }
+    }
+    public static void main(String[] args) {
+        Basquete_Robo b = new Basquete_Robo();
+        b.Distancia(1720);
+        b.PontLancamento();
+    }
+}
---
+++ Fig.java
@@ -0,0 +1,4 @@
+package estdados;
+public class Fig {
+    int valor;
+}
